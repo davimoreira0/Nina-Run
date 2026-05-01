c57e46838d20b87c41399c98338ae87b21dfc706
Davi Moreira Pereira <moreiradavi336@gmail.com>
2026-05-01 16:54:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 sudo apt update
-sudo apt install -y nmap dirb whois whatweb host traceroute medusa hping3 lbd skipfish sslscan sqlinjection sublist3r theharvester nikto cewl ssh metagoofil netdiscover
+sudo apt install -y nmap dirb whois whatweb host traceroute medusa hping3 lbd skipfish sslscan sublist3r theharvester nikto cewl ssh metagoofil netdiscover
